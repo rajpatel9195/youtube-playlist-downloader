49c93a96bb50dc0e160c85823f33a3299e47763e
RAJ PATEL <89418628+rajpatel9195@users.noreply.github.com>
2022-08-30 01:02:43 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -3,12 +3,11 @@ from pytube.cli import on_progress
 url = input("url ")
 filename = input("filename ")
 pl = Playlist(url)
-
+print("Totle no. of videos" +  "  " + str(pl.length))
 x=1
 for vid in pl.videos:
     try:
-
-        print(x," is dwonloading  ")
+        print(x , " is dwonloading... "  ,  "File size: " , str(round( vid.streams.get_highest_resolution().filesize / (1024 * 1024), 3)) , " MB")    
         print(vid.title)
         vid.register_on_progress_callback(on_progress)
         vid.streams.get_highest_resolution().download(filename)
